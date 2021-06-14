d8209aa13e44b07104e8e5eba2d7760c042dcbe0
Lucas PIres <luc_50@outlook.com>
2021-06-14 11:05:04 -0300

    forca: estipulando tentativas de erros

--- jogos/forca.py
+++ jogos/forca.py
@@ -6,11 +6,12 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
     letras_acertadas = ["_","_","_","_","_","_"]
 
     enforcou = False
     acertou = False
+    erros_contador = 0
 
     print(letras_acertadas)
 
@@ -18,15 +19,29 @@ def jogar():
 
         chute = raw_input("Qual letra? ")
         # tira os espaços da string no inicio e no final
-        chute = chute.strip()
+        chute = chute.strip().upper()
         
-        index = 0
-        for letra in palavra_secreta:
-            # upper muda as letras para maiuscula
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index += 1
-        
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                # upper muda as letras para maiuscula
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros_contador += 1
+
+        # compara se erros_contador é igual a 6, se sim, encerra o jogo
+        enforcou = erros_contador == 6
+
+        # o "_" não devieria estar dentro das letras acertadas
+        acertou = "_" not in letras_acertadas
+
+        if(acertou):
+            print("Você ganhou.")
+        else:
+            print("Você perdeu.")
+
         print(letras_acertadas)
 
     print("Fim do jogo")
